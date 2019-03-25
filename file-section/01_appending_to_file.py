c4cc02faf38b846dcce6ccc40b747200bf26d496
jordanhudgens <jordan@devcamp.com>
2019-03-25 11:39:27 -0400

    C_03_appending_to_python_files

--- file-section/01_appending_to_file.py
+++ file-section/01_appending_to_file.py
@@ -1,9 +1,11 @@
 import datetime
 from time import sleep
 
-for i in range(100):
+for i in range(5):
     file_builder = open("logger.txt", "a+")
     file_builder.write(f'{datetime.datetime.now()}\n')
     file_builder.close()
 
-    sleep(2.0)
+    print("file created")
+
+    sleep(1)
